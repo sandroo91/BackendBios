57897e09807f338db73e7217a07da5b2b436874a
RobertKuijpers <43451180+RobertKuijpers@users.noreply.github.com>
2018-11-03 20:26:01 +0100

    ERD aangevuld, moet nog DAO's bij

--- src/main/java/bios/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/bios/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -4,11 +4,17 @@ package bios.springframework.spring5webapp.bootstrap;
 import bios.springframework.spring5webapp.model.Zaal;
 import bios.springframework.spring5webapp.model.Film;
 import bios.springframework.spring5webapp.model.Tijden;
+import bios.springframework.spring5webapp.model.Dag;
 import bios.springframework.spring5webapp.model.Voorstelling;
+import bios.springframework.spring5webapp.model.Stoel;
+import bios.springframework.spring5webapp.model.Reservering;
 import bios.springframework.spring5webapp.repositories.ZaalRepository;
 import bios.springframework.spring5webapp.repositories.FilmRepository;
 import bios.springframework.spring5webapp.repositories.TijdRepository;
+import bios.springframework.spring5webapp.repositories.DagRepository;
 import bios.springframework.spring5webapp.repositories.VoorstellingRepository;
+import bios.springframework.spring5webapp.repositories.StoelRepository;
+import bios.springframework.spring5webapp.repositories.ReserveringRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -19,14 +25,20 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     private ZaalRepository zaalRepository;
     private FilmRepository filmRepository;
     private TijdRepository tijdRepository;
+    private DagRepository dagRepository;
     private VoorstellingRepository voorstellingRepository;
+    private StoelRepository stoelRepository;
+    private ReserveringRepository reserveringRepository;
 
 
-    public DevBootstrap(ZaalRepository zaalRepository, FilmRepository filmRepository, TijdRepository tijdRepository, VoorstellingRepository voorstellingRepository) {
+    public DevBootstrap(ZaalRepository zaalRepository, FilmRepository filmRepository, TijdRepository tijdRepository,DagRepository dagRepository, VoorstellingRepository voorstellingRepository, StoelRepository stoelRepository, ReserveringRepository reserveringRepository) {
         this.zaalRepository = zaalRepository;
         this.filmRepository = filmRepository;
         this.tijdRepository = tijdRepository;
+        this.dagRepository = dagRepository;
         this.voorstellingRepository = voorstellingRepository;
+        this.stoelRepository = stoelRepository;
+        this.reserveringRepository = reserveringRepository;
 
     }
 
--- src/main/java/bios/springframework/spring5webapp/controllers/VoorstellingController.java
+++ src/main/java/bios/springframework/spring5webapp/controllers/VoorstellingController.java
@@ -12,7 +12,7 @@ import java.util.List;
 /**
  * Created by jt on 5/18/17.
  */
-@CrossOrigin(origins = { "http://localhost:8080",  "http://localhost:8081"})
+@CrossOrigin(origins = { "http://localhost:8080",  "http://localhost:8081" })
 @RestController
 @RequestMapping("/voorstelling")
 public class VoorstellingController {
