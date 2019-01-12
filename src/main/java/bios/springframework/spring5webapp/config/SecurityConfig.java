6358e81b2af4069d69b2479c9656431a2e247832
Majnkeyzer <43861087+Majnkeyzer@users.noreply.github.com>
2019-01-12 09:57:33 +0100

    Add files via upload

---
+++ src/main/java/bios/springframework/spring5webapp/config/SecurityConfig.java
@@ -0,0 +1,89 @@
+package bios.springframework.spring5webapp.config;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import bios.springframework.spring5webapp.auth.CustomSimpleUrlAuthenticationSuccessHandler;
+import bios.springframework.spring5webapp.auth.RestAuthenticationEntryPoint;
+
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    private CustomSimpleUrlAuthenticationSuccessHandler mySuccessHandler = new CustomSimpleUrlAuthenticationSuccessHandler();
+    private SimpleUrlAuthenticationFailureHandler myFailureHandler = new SimpleUrlAuthenticationFailureHandler();
+
+    @Autowired
+    private CorsFilter corsFilter;
+
+    // add a reference to our security data source
+
+    @Autowired
+    private DataSource securityDataSource;
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // use jdbc authentication ... !!!
+
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+
+    }
+
+
+//    @Override
+//    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("admin").password(encoder().encode("password")).roles("ADMIN")
+//                .and()
+//                .withUser("user").password(encoder().encode("password")).roles("USER");
+//    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .addFilterBefore(corsFilter, ChannelProcessingFilter.class)
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and()
+                .formLogin().loginPage("/Login").permitAll()
+                .successHandler(mySuccessHandler)
+                .failureHandler(myFailureHandler)
+                .and()
+                .logout();
+    }
+//                .antMatchers("/login").permitAll()
+//                .and()
+//                .formLogin()
+//                .successHandler(mySuccessHandler)
+//                .failureHandler(myFailureHandler)
+//                .and()
+//                .logout();
+//    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
